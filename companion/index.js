c5542a1e1921639accd48422d166ec3023f9d77d
iph33r <40376457+iph33r@users.noreply.github.com>
2022-01-13 22:54:35 -0800

    Update index.js

--- companion/index.js
+++ companion/index.js
@@ -1,1 +1,44 @@
+import * as messaging from "messaging";
+import { settingsStorage } from "settings";
 
+// Fetch Nutrition Data from Fitbit Web API
+function fetchData(accessToken)  {
+  let date = new Date();
+  let todayDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`; //YYYY-MM-DD
+
+  // Nutrition API docs - https://dev.fitbit.com/reference/web-api/nutrition/
+  fetch(`https://api.fitbit.com/1/user/-/foods/log/date/${todayDate}.json`, {
+    method: "GET",
+    headers: {"Authorization": `Bearer ${accessToken}`}
+  })
+  .then(function(res) {return res.json();})
+  .then(function(data) {
+    if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+      messaging.peerSocket.send(data);
+    }
+  })
+  .catch(err => console.log('[FETCH]: ' + err));
+}
+
+
+// A user changes Settings
+settingsStorage.onchange = evt => {
+  if (evt.key === "oauth") {
+    // Settings page sent us an oAuth token
+    let data = JSON.parse(evt.newValue);
+    fetchData(data.access_token) ;
+  }
+};
+// Restore previously saved settings and send to the device
+function restoreSettings() {
+  for (let index = 0; index < settingsStorage.length; index++) {
+    let key = settingsStorage.key(index);
+    if (key && key === "oauth") {
+      // We already have an oauth token
+      let data = JSON.parse(settingsStorage.getItem(key))
+      fetchData(data.access_token);
+    }
+  }
+}
+// Message socket opens
+messaging.peerSocket.onopen = () => {restoreSettings();};
