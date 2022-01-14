b19154cb7c61dc6808d84c42a460c704c467e304
iph33r <40376457+iph33r@users.noreply.github.com>
2022-01-13 22:59:17 -0800

    Update index.jsx

--- settings/index.jsx
+++ settings/index.jsx
@@ -1,1 +1,23 @@
 
+
+function mySettings(props) {
+  return (
+    <Page>
+      <Section title={<Text bold align="center">Fitbit Account</Text>}>
+        <Oauth
+          settingsKey="oauth"
+          title="Login"
+          label="Fitbit"
+          status="Login"
+          authorizeUrl="https://www.fitbit.com/oauth2/authorize"
+          requestTokenUrl="https://api.fitbit.com/oauth2/token"
+          clientId="[obfuscated]"
+          clientSecret="[obfuscated]"
+          scope="nutrition weight heartrate"
+        />
+      </Section>
+    </Page>
+  );
+}
+
+registerSettingsPage(mySettings);
